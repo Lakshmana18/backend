895eb25f19780052e832bbf091a6cc222a0c5dcd
Lakshmana18 <lakshmanannamneedi143@gmail.com>
2024-08-08 21:08:55 +0530

    8/8

--- backend/src/server1.js
+++ backend/src/server1.js
@@ -1,6 +1,6 @@
 import cors from 'cors';
 import express from 'express';
-import { connectToDB } from "./db.js";
+import { connectToDB, db } from "./db.js";
 
 const app = express()
 app.use(cors())
@@ -9,10 +9,64 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.json("server is running successfully!");
 })
-
-
+app.post('/insert', async(req, res) => {
+    await db.collection("ast").insertOne({Name:req.body.name,Team:req.body.team})
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((e)=>console.log(e))
+})
+app.post('/insertmany', async(req, res) => {
+    await db.collection("ast").insertMany([req.body])
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((e)=>console.log(e))
+})
+app.post('/findone', async(req, res) => {
+    await db.collection("ast").findOne({Name:"Kousik"})
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((e)=>console.log(e))
+})
+app.post('/find', async(req, res) => {
+    await db.collection("ast").find().toArray()
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((e)=>console.log(e))
+})
+app.post('/deleteone', async(req, res) => {
+    await db.collection("ast").deleteOne({Name:"Hari",gender:"male"})
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((e)=>console.log(e))
+})
+app.post('/deletemany', async(req, res) => {
+    await db.collection("Ammu").deleteMany()
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((e)=>console.log(e))
+})
+app.post('/updateone', async(req, res) => {
+    await db.collection("ast").updateOne({Name:"Hari"},{$set:{salary:"20k"}})
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((e)=>console.log(e))
+})
+app.post('/updatemany', async(req, res) => {
+    await db.collection("ast").updateMany({Name:"Hari"},{$set:{grade:"A",hike:"2k"}})
+    .then((result)=>{
+        res.json(result)
+    })
+    .catch((e)=>console.log(e))
+})
 connectToDB(() => {
-    app.listen(9000, () => {
-        console.log("server running at 9000");
+    app.listen(9001, () => {
+        console.log("server running at 9001");
     })
 })
